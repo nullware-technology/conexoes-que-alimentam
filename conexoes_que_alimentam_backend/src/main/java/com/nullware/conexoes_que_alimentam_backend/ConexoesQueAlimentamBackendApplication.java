204935c8dcb4f0906e4a5e432eb6f494784643ff
MateusVasc <mvaraujo2004@gmail.com>
2025-05-11 15:31:51 -0300

    feat: adds backend structure

---
+++ conexoes_que_alimentam_backend/src/main/java/com/nullware/conexoes_que_alimentam_backend/ConexoesQueAlimentamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nullware.conexoes_que_alimentam_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexoesQueAlimentamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexoesQueAlimentamBackendApplication.class, args);
+	}
+
+}
