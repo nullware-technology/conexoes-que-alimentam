70db32892a8cd72b46e79e109963ef197b465c33
MateusVasc <mvaraujo2004@gmail.com>
2025-05-11 15:32:38 -0300

    feat: adds address entity

---
+++ conexoes_que_alimentam_backend/src/main/java/com/nullware/conexoes_que_alimentam_backend/entities/Address.java
@@ -0,0 +1,54 @@
+package com.nullware.conexoes_que_alimentam_backend.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "addresses")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String zipCode;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @ManyToOne
+    @JoinColumn(name = "donor_organization_id")
+    private DonorOrganization donorOrganization;
+
+    @ManyToOne
+    @JoinColumn(name = "social_organization_id")
+    private SocialOrganization socialOrganization;
+}
